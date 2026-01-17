ea58df104b01f0ef3ccdf39bcdae91d75cc978b2
unknown <kszadejko@edu.cdv.pl>
2026-01-17 12:35:20 +0100

    Cleanup old prototype and project refactor

--- Female.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DogBreedingManager;
-
-public class Female : Dog //dziedziczenie - po klasie Dog
-{
-    public Female()  //polimorfizm - nadpisujemy Sex z klasy bazowej Dog
-    {
-        Sex = "Female"; 
-    }
-}
--- Male.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DogBreedingManager;
-
-public class Male : Dog //dziedziczenie - klasa Male dziedziczy po Dog
-{
-    public Male() // polimorfizm - nadpisujemy właściwość Sex z klasy bazowej Dog
-    {
-        Sex = "Male";
-    }
-}
